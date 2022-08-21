8bd0e416eb8cdc9067a9ff21dc345694f3e398eb
Mikhail <kostrominmi@mail.ru>
2022-08-21 16:50:31 +0300

    task 8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= N)
+{
+  if (a % 2 == 0) 
+  {
+    Console.Write (a + ", ");
+  }
+a = a +1;
+} 
+
+
