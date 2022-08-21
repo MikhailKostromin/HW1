ac51a92d32ff0edb1902b06bc8142f22b394fd6b
Mikhail <kostrominmi@mail.ru>
2022-08-21 17:04:08 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,10 +4,12 @@ Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a>b)
 {
+    Console.Write ("Большее число, ");
     Console.WriteLine(a);
 } 
 else if(b>a)
 {
+   Console.Write ("Большее число, ");
    Console.WriteLine(b);
 }  
 else 
