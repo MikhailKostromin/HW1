a06f5b9c56980a3653efbb457297181ce43650e5
Mikhail <kostrominmi@mail.ru>
2022-08-21 18:04:48 +0300

    task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,18 +2,12 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите третье число");
 int c = Convert.ToInt32(Console.ReadLine());
-int Max = a;
-if (b>Max)
-{
-    Console.WriteLine(b);
-} 
-else if(c>Max)
-{
-   Console.WriteLine(c);
-}  
-else 
-{
-   Console.WriteLine(a);
-} 
+int Max=a;
+if (b>Max) Max=b;
+if (c>Max) Max=c;
+
+Console.Write("Максимальное число, ");
+Console.WriteLine(Max);
+
